1d5f828a22498391c78a6cf3a149420a6d99c4df
Kaitlyn Metler <ksmetler@gmail.com>
2024-11-21 13:36:58 -0700

    Add DNS CNAME record

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 sudo certbot -n -d encrypttest.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email ksmetler@gmail.com
+sudo certbot -n -d minervascleaningservice.is404.net --nginx --agree-tos --email ksmetler@gmail.com
