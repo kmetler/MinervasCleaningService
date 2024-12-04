9c530297fbdda4e3660ff16f839c3f66972b4b33
Kaitlyn Metler <ksmetler@gmail.com>
2024-12-04 10:40:46 -0700

    random

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ let path = require("path");
 
 const port = process.env.PORT || 5000;
 
+// authentication
+const session = require("express-session");
+
+app.use(
+    session({
+        secret : "my_secret_key", //replace with a secure, random key
+        resave : false,
+        saveUninitialized : false,
+        cookie : {secure : false, maxAge : 3600000}, // session expires in 1 hour (adjust as needed)
+    })
+);
+
 app.set("view engine", "ejs");
 
 app.set("views", path.join(__dirname, "views"));
@@ -33,6 +45,24 @@ app.get("/", async (req,res) => {
 
 app.get("/showLogin", (req, res) => res.render("login"));
 
-app.post("/login", (req, res) => res.render("clientpage"));
+app.post("/login", async (req, res) => {
+    const { username, password } = req.body
+
+    try {
+        const credentials = await knex('credentials')
+            .select('*')
+            .where({ username, password }) //Replace with hashedpassword comparison in prodection
+            .first();
+        if (credentials) {
+            req.session.isAuthenticated = true; // set session authentication flag
+        } else {
+            req.session.isAuthenticated = false; // clear authenticaion flag
+        }
+    } catch (error) {
+        console.error("Error during login:", error);
+        res.status(500).send("Database query failed:" + error.message);
+    }
+    res.render("clientpage");
+});
 
 app.listen(port, () => console.log("Express App has started listening."));
